e93c0d92040d11dde3182f3c78fee24bf7c13f5a
MARC NAVARRO <navamarc@gmail.com>
2025-03-31 01:06:10 +0200

    transfert widget package to root package

--- app/src/main/java/com/openclassrooms/notes/ui/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/notes/ui/MainActivity.kt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.openclassrooms.notes.ui
 
 import android.os.Bundle
@@ -7,98 +6,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.openclassrooms.notes.R
-import com.openclassrooms.notes.data.model.Note
 import com.openclassrooms.notes.databinding.ActivityMainBinding
-import com.openclassrooms.notes.ui.widget.NoteItemDecoration
 import com.openclassrooms.notes.ui.widget.NotesAdapter
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-
-/**
- * The main activity for the app.
- */
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    /**
-     * The binding for the main layout.
-     */
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
-    private val notesAdapter = NotesAdapter(emptyList())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setupViewModel()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        initRecyclerView()
-        initFABButton()
-        collectNotes()
-    }
-
-    private fun setupViewModel() {
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-//        viewModel.tasks.observe(this, ::updateNoteList)
-    }
-
-    /**
-     * Initializes the RecyclerView.
-     */
-    private fun initRecyclerView() {
-        with(binding.recycler) {
-            addItemDecoration(
-                NoteItemDecoration(
-                    resources.getDimensionPixelSize(R.dimen.default_margin),
-                    resources.getInteger(R.integer.span_count)
-                )
-            )
-            adapter = notesAdapter
-        }
-    }
-
-    /**
-     * Initializes the FAB button.
-     */
-    private fun initFABButton() {
-        binding.btnAdd.setOnClickListener {
-            MaterialAlertDialogBuilder(this).apply {
-                setTitle(R.string.coming_soon)
-                setMessage(R.string.not_available_yet)
-                setPositiveButton(android.R.string.ok, null)
-            }.show()
-        }
-    }
-
-    /**
-     * Observe notes from ViewModel
-     */
-    private fun collectNotes() {
-        lifecycleScope.launch {
-            viewModel.notes.collect { notesList ->
-                notesAdapter.updateNotes(notesList)
-            }
-        }
-    }
-//    private fun updateNoteList(notes: List<Note>) {
-//        adapter.submitList(notes)
-//    }
-
-}
-=======
-package com.openclassrooms.notes.ui
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.openclassrooms.notes.R
-import com.openclassrooms.notes.data.model.Note
-import com.openclassrooms.notes.databinding.ActivityMainBinding
 import com.openclassrooms.notes.widget.NoteItemDecoration
-import com.openclassrooms.notes.widget.NotesAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -174,4 +84,3 @@ class MainActivity : AppCompatActivity() {
 //    }
 
 }
->>>>>>> 73203077f385f2c8c358af948e58a0633883c9b9
---
+++ app/src/main/java/com/openclassrooms/notes/widget/NotesAdapter.kt
@@ -0,0 +1,38 @@
+package com.openclassrooms.notes.ui.widget
+
+import android.annotation.SuppressLint
+import com.openclassrooms.notes.widget.NoteViewHolder
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.notes.data.model.Note
+import com.openclassrooms.notes.databinding.NoteBinding
+
+/**
+ * An adapter for displaying a list of notes in a RecyclerView.
+ * @param notes The list of notes to display.
+ */
+class NotesAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NoteViewHolder>() {
+
+    /**
+     * Updates the list of notes displayed by the adapter.
+     * @param newNotes The new list of notes to display.
+     */
+    fun updateNotes(newNotes: List<Note>) {
+        notes = newNotes
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = NoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int =
+        notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.bind(note)
+    }
+}
